704fe2f394ff049983d5e1b85264bef0ebbb5d60
hhhhhdaliyuan <2156374506@qq.com>
2026-07-16 15:37:35 +0800

    chore(main): add FreeRTOS and ESP-IDF system headers

--- main/main.c
+++ main/main.c
@@ -1,4 +1,12 @@
 #include <stdio.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "esp_system.h"
+#include "esp_spi_flash.h"
+#include "esp_log.h"
+#include "esp_event.h"
+#include "esp_netif.h"
+#include "esp_wifi.h"
 
 void app_main(void)
 {
